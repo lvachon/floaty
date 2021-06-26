6734ba1ede19a8aa831097855cc043fc27050f54
Luc Vachon <lvachon@gmail.com>
2021-06-25 21:09:45 -0400

    now with gps exif!

--- cam.sh
+++ cam.sh
@@ -3,4 +3,4 @@
 DATETIMESTR="$(date +"%Y-%m-%d-%R")"
 mkdir $DATETIMESTR
 echo $DATETIMESTR
-raspistill -tl 1000 -t 0 -o ./$DATESTR/cam%04d.jpg -w 1024 -h 768 -q 10 -l /var/www/html/latest.jpg &
+raspistill -tl 1000 -t 0 -o ./$DATETIMESTR/cam%04d.jpg -w 1024 -h 768 -q 10 -l /var/www/html/latest.jpg -gps &
