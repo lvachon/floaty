a5abe4dc5a73e6e419634a142020398de15a8b44
Luc Vachon <lvachon@gmail.com>
2021-06-25 20:51:15 -0400

    Better interface, light toggle, and update to cam command so it won't overwrite it's old pics and will also now autobalance and autoexpose

---
+++ cam.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+DATETIMESTR="$(date +"%Y-%m-%d-%R")"
+mkdir $DATETIMESTR
+echo $DATETIMESTR
+raspistill -tl 1000 -t 0 -o ./$DATESTR/cam%04d.jpg -w 1024 -h 768 -q 10 -l /var/www/html/latest.jpg &
